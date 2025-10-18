3697a003dec1217ffafa556ed7e3556c61b19b88
何广清 <guangqing_h@163.com>
2025-10-18 18:00:46 +0800

    add by ghe

--- easyexcel-test/src/test/java/com/alibaba/easyexcel/test/ghe/demo2/DemoData.java
+++ easyexcel-test/src/test/java/com/alibaba/easyexcel/test/ghe/demo2/DemoData.java
@@ -1,5 +1,6 @@
 package com.alibaba.easyexcel.test.ghe.demo2;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import java.util.Date;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -9,7 +10,10 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode
 public class DemoData {
+    @ExcelProperty("字符串标题")
     private String string;
+    @ExcelProperty("日期标题")
     private Date date;
+    @ExcelProperty("数字标题")
     private Double doubleData;
 }
--- easyexcel-test/src/test/java/com/alibaba/easyexcel/test/ghe/demo2/Test.java
+++ easyexcel-test/src/test/java/com/alibaba/easyexcel/test/ghe/demo2/Test.java
@@ -1,20 +1,17 @@
 package com.alibaba.easyexcel.test.ghe.demo2;
 
-import com.alibaba.easyexcel.test.util.TestFileUtil;
 import com.alibaba.excel.EasyExcel;
-import com.alibaba.excel.ExcelReader;
-import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.read.listener.PageReadListener;
-import com.alibaba.excel.read.listener.ReadListener;
-import com.alibaba.excel.read.metadata.ReadSheet;
-import com.alibaba.excel.util.ListUtils;
 import com.alibaba.fastjson2.JSON;
-import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Test {
+
   /**
    * 最简单的读
    * <p>
@@ -26,14 +23,18 @@ public class Test {
    */
   @org.junit.jupiter.api.Test
   public void simpleRead() {
-    String fileName = "/Users/heguangqing/IdeaProjects/easyexcel/easyexcel-test/src/test/resources/demo/demo.xlsx";
-    // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
-    // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
-    EasyExcel.read(fileName, DemoData.class, new PageReadListener<DemoData>(dataList -> {
-      for (DemoData demoData : dataList) {
-        log.info("读取到一条数据{}", JSON.toJSONString(demoData));
-      }
-    })).sheet().doRead();
-
+//    String fileName = "/Users/heguangqing/IdeaProjects/easyexcel/easyexcel-test/src/test/resources/demo/demo.xlsx";
+//    // 这里默认每次会读取100条数据 然后返回过来 直接调用使用数据就行
+//    // 具体需要返回多少行可以在`PageReadListener`的构造函数设置
+//    EasyExcel.read(fileName, DemoData.class, new PageReadListener<DemoData>(dataList -> {
+//      for (DemoData demoData : dataList) {
+//        log.info("读取到一条数据{}", JSON.toJSONString(demoData));
+//      }
+//    })).sheet().doRead();
+    Map<String, List<String>> courseMap = new HashMap<>();
+    courseMap.computeIfAbsent("数学", k -> new ArrayList<>()).add("小明");
+    courseMap.computeIfAbsent("英语", k -> new ArrayList<>()).add("小红");
+    courseMap.computeIfAbsent("数学", k -> new ArrayList<>()).add("小刚");
+    System.out.println(courseMap);
   }
 }
